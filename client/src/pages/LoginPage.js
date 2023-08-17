9d6bacd915212a82990539fe074c540e2d4e4853
Aditya Dahake <dahakeadi15@gmail.com>
2023-08-17 23:29:11 +0530

    add style to login and register pages

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,3 +1,12 @@
 export default function LoginPage() {
-  return <div>Login Page</div>;
+  return (
+    <div>
+      <form action="" className="login">
+        <h1>Login</h1>
+        <input type="text" placeholder="username" />
+        <input type="password" name="" id="" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,3 +1,12 @@
 export default function RegisterPage() {
-  return <div>Register Page</div>;
+  return (
+    <div>
+      <form action="" className="register">
+        <h1>Register</h1>
+        <input type="text" placeholder="username" />
+        <input type="password" name="" id="" placeholder="password" />
+        <button>Register</button>
+      </form>
+    </div>
+  );
 }
