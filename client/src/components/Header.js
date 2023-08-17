9d78fe5e114c7ba918cb142295b215a0dd370b5c
Aditya Dahake <dahakeadi15@gmail.com>
2023-08-17 23:01:39 +0530

    Create components

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <a href="/" className="logo">
+        MyBlog
+      </a>
+      <nav>
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+      </nav>
+    </header>
+  );
+}
